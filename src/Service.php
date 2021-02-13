7811be021345a49ca6220a841b0d692cbf27b767
ArtyomVorontsov <70565753+ArtyomVorontsov@users.noreply.github.com>
2021-02-13 11:56:10 +0200

    Create Service.php

---
+++ src/Service.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    "db" => [
+        "host" => "YOUR_HOST_NAME",
+        "dbname" => "YOUR_DB_NAME",
+        "username" => "YOUR_DB_USERNAME",
+        "password" => "YOUR_DB_PASSWORD"
+    ]
+]
+
+?>
