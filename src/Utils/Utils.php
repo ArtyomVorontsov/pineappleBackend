007b861e5a4d3822c02081f4472381ff875d2bb4
Artjoms <artv20022@gmail.com>
2021-02-15 09:07:18 +0200

    added utils and reduced amount of code by refactoring

--- src/Utils/Utils.php
+++ src/Utils/Utils.php
@@ -7,12 +7,14 @@
         public static function parseArrayToJson($dataArray){
             //parsing array to JSON
             $dataJSON = "";
+
             foreach ($dataArray as $data)
             $dataJSON = $dataJSON . "," . $data->getJSON();
             
             //remove first character from dataJSON because it is: ","
             $dataJSON = substr($dataJSON, 1);
             $dataJSON = "[" . $dataJSON . "]";
+           
             return $dataJSON;
         }
     }
