8c3168133db5ee10ce1c36f2b18e205baabe28c9
Artjoms <artv20022@gmail.com>
2021-02-15 09:14:54 +0200

    added error handling

--- src/Db.php
+++ src/Db.php
@@ -1,20 +1,27 @@
 <?php
     namespace src;
 
-
+use Exception;
+use src\Exceptions\WrongUserInputException;
 
 class Db {
 
         private $_pdo;
 
         private function __construct(){
-            $dbPropertiesArray = require __DIR__ . "/Service.php";
-            $dbProperties =  $dbPropertiesArray["db"];
-            $dsn =  "mysql:host=" . $dbProperties["host"] .  ";dbname=" . $dbProperties["dbname"];
-
-            $this->_pdo = new \PDO($dsn, $dbProperties["username"], $dbProperties["password"]);
-            $this->_pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-            $this->_pdo->exec("SET NAMES UTF8");
+            try{
+                $dbPropertiesArray = require __DIR__ . "/Service.php";
+                $dbProperties =  $dbPropertiesArray["db"];
+                $dsn =  "mysql:host=" . $dbProperties["host"] .  ";dbname=" . $dbProperties["dbname"];
+    
+                $this->_pdo = new \PDO($dsn, $dbProperties["username"], $dbProperties["password"]);
+                $this->_pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+                $this->_pdo->exec("SET NAMES UTF8");
+            }catch(Exception $e){
+                throw new WrongUserInputException("Database connection error, check Service.php to setup your db connection credentials. 
+                Also check if PDO MySQL driver is running.");
+            }
+            
         }
 
         
